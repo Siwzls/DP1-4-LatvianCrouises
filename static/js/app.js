163537de140832e4b4ff0ccb9396c2279261cb9f
BlackJeff <x>
2021-06-08 01:33:27 +0300

    Minor changes in js file

--- static/js/app.js
+++ static/js/app.js
@@ -9,34 +9,29 @@ const popupDetails = document.querySelector('#popup_details');
 const popupLogin = document.querySelector('#popup_login');
 const popupEdit = document.querySelector('#popup_edit');
 
-// Shows details popup
-if(btnMoreDetails != null){
-    for(i = 0; i < btnMoreDetails.length; i++){
-        btnMoreDetails[i].addEventListener('click', (e) =>{
-            switchPopup(popupDetails);
-        });
+document.addEventListener('click', (e) =>{
+    for(i = 0; i < btnMoreDetails.length + btnEdit.length; i++)
+    {
+        switch(e.target){
+            case btnMoreDetails[i]:
+                switchPopup(popupDetails);
+                break;
+            case btnLogin:
+                switchPopup(popupLogin);
+                break;
+            case btnEdit[i]:
+                switchPopup(popupEdit);
+                break;
+        }
     }
-}
-// Shows login popup
-if(btnLogin != null){
-    btnLogin.addEventListener('click', (e) =>{
-        switchPopup(popupLogin);
-    });
-}
-//Shows edit popup
-if(btnEdit != null){
-    for(i = 0; i < btnEdit.length; i++){
-        btnEdit[i].addEventListener('click', (e) =>{
-            switchPopup(popupEdit);
-        });
-    }
-}
+})
 // Hide popup
 for(i = 0; i < btnPopupClose.length; i++){
 btnPopupClose[i].addEventListener('click', (e) =>{
     var popupId = e.target.parentNode.parentNode;
     switchPopup(popupId);
 });
+}
 document.addEventListener('click', (e) =>{
     switch(e.target.id){
         case 'ports':
@@ -65,7 +60,6 @@ document.addEventListener('click', (e) =>{
             break;
     }
 })
-}
 function switchPopup(popupToClose){
     if(popupToClose.style.display == "flex")
         popupToClose.style.display = "none";
