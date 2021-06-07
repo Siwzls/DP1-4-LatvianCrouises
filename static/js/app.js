6af96131081dc8dade6bd780843e4fea45a57d1c
BlackJeff <x>
2021-06-07 15:38:56 +0300

    Some changes of js function for popups

--- static/js/app.js
+++ static/js/app.js
@@ -1,21 +1,23 @@
 const btnMoreDetails = document.querySelectorAll('.btn_details');
-const btnPopupClose = document.querySelector('#btn_popupClose');
+const btnPopupClose = document.querySelectorAll('.btn_popupClose');
 const popupDetails = document.querySelector('#popup_details');
 
-for(i = 0; i < btnMoreDetails.length; i++)
-{
+// Shows details popup
+for(i = 0; i < btnMoreDetails.length; i++){
     btnMoreDetails[i].addEventListener('click', (e) =>{
         popupDetails.style.display = "flex";
     });
 }
-
-btnPopupClose.addEventListener('click', (e) =>{
-    switchPopup(popupDetails);
-})
-
+// Hide popup
+for(i = 0; i < btnPopupClose.length; i++){
+btnPopupClose[i].addEventListener('click', (e) =>{
+    var popupId = e.target.parentNode.parentNode;
+    switchPopup(popupId);
+});
+}
 function switchPopup(popupToClose){
     if(popupToClose.style.display = "flex")
         popupToClose.style.display = "none";
     else if(popupToClose.style.display = "none")
-        popupToClose.style.display = "flex"
+        popupToClose.style.display = "flex";
 }
